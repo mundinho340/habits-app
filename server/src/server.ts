fc975687c95cd06d37a5137c9d56ab77314b8ee9
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-16 22:29:22 -0200

    setApp

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,15 @@
-console.log("oi Raimundo")
+import fastify from "fastify";
+
+const app = fastify()
+
+
+app.get('/', ()=>{
+   return "hello world"
+})
+
+
+app.listen({
+   port:8080
+}).then(()=>{
+   console.log("server running!")
+})
