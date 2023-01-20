f4a13b4b2c31164882153640bfba0f3ff021554e
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-20 20:40:13 -0300

    seed

---
+++ server/prisma/seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+    await prisma.habit.deleteMany()
+    await prisma.habit.create({
+        data: {
+            title:"beber 2L de agua",
+            created_at: new Date("2023-01-20-10T00:00:000z")
+        }
+    })
+  }
+ 
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  }
+  )
