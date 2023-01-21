cfb116a78fffbf455e7a3b3215cb6a43d1939cfe
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-20 22:36:35 -0300

    config-auth

--- server/src/routs.ts
+++ server/src/routs.ts
@@ -1,10 +1,35 @@
 import {prisma} from "./lib/prisma"
 import { FastifyInstance } from "fastify" 
+import {z} from 'zod'
 
 export async function appRouts(app: FastifyInstance){
-    app.get('/', async ()=>{
+    app.get('/', async (request)=>{
     const habits  = await  prisma.habit.findMany({
-      
+      app.post('/habits', async (request)=>{
+        const createHabitBody = z.object({
+            title: z.string(),
+            WeekDays: z.array(
+                z.number().min(0).max(6)
+            )
+        })
+        const {title, WeekDays}= createHabitBody.parse(request.body)
+
+        await prisma.habit.create({
+            data:{
+                title,
+                created_at: new Date(),
+                WeekDays:{
+                    create: WeekDays.map(WeekDay=>{
+                        return{
+                            week_day:WeekDay,
+                        }
+                    })
+                }
+            }
+        })
+      })
+
+
     })
     return habits
  })
