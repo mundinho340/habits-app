946076a8c7e6de17477774b5a7a7820e71b5a5c6
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-02-02 21:44:28 -0300

    Update routs.ts

--- server/src/routs.ts
+++ server/src/routs.ts
@@ -135,15 +135,18 @@ export async function appRouts(app: FastifyInstance){
             (
                 SELECT
                     cast(count(*) as float) 
-                    FROM day_habits DH
+                    FROM day_habits DH  
                     WHERE DH.day_id =D.id
             ) as completed,
             (
                 SELECT 
                 cast(count(*) as float)
                 FROM habit_week_days HWD
+                JOIN habits H
+                ON H.id =  HWD.habit_id
                 WHERE 
                 HWD.week_day =cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+                AND H.created_at <= D.date
             ) as amount
             FROM days D
         `
