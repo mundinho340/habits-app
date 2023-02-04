7c80d1ef9cd705a4032ad9f37ce0b5b546fdee28
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-02-03 21:38:49 -0300

    habit_uix

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,26 +1,30 @@
 import {Plus} from 'phosphor-react'
+import * as Dialog from '@radix-ui/react-dialog'
 //import { Habits } from './components/Habits'
 import logoImage from '../assets/logo.svg' 
 import { useState } from 'react'
 
 export function Header(){
-  const [isModalOpen, setIsModalOpen]=useState(false)
     return(
         <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
           <img src={logoImage} alt="Habits" />
+          <Dialog.Root>
+            <Dialog.Trigger
+            type='button'
+            className='border border-violet-500 font-semibold rounded-lg px-6 py-4
+            flex justify-between gap-3 hover:border-violet-300 items-center'
+            >
+              <Plus size={20} className="text-violet-500"/>
+              Novo Habito
+            </Dialog.Trigger>
+            <Dialog.Portal>
+              <Dialog.Overlay className='w-screen h-screen bg-black/88 fixed insert-0'>
+                Conteudo do modal
+              </Dialog.Overlay>
+              
+            </Dialog.Portal>
+          </Dialog.Root>
           
-        <button
-        type='button'
-        className='border border-violet-500 font-semibold rounded-lg px-6 py-4
-        flex justify-between gap-3 hover:border-violet-300 items-center'
-        onClick={() => {
-          alert('clickou')
-          setIsModalOpen(true)
-        }}
-        >
-          <Plus size={20} className="text-violet-500"/>
-          Novo Habito
-        </button>
     </div>
     )
 }
