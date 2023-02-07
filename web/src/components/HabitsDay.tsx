1c4144c3fb60dd6fee9ea5403df68d356131d5e3
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-02-07 19:01:22 -0300

    Update HabitsDay.tsx

--- web/src/components/HabitsDay.tsx
+++ web/src/components/HabitsDay.tsx
@@ -10,8 +10,8 @@ export  function HabitsDay(){
         <Popover.Trigger className='w-10 h-10 bg-zinc-900 border-zinc-800 rounded-lg'/>
         <Popover.Portal>
             <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
-                <span>terça-feira</span>
-                <span>07/02</span>
+                <span className='font-semibold text-zinc-400'>terça-feira</span>
+                <span className='at-1 font-extrabold leading-light text-3xl'>07/02</span>
                 <Popover.Arrow height={8} width={16} className='fill-zinc-900'/>
             </Popover.Content>
         </Popover.Portal>
