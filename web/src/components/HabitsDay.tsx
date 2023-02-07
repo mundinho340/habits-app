86d8fcb6d40413ed671d2c072664dfaacfff964f
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-02-07 18:36:25 -0300

    Update HabitsDay.tsx

--- web/src/components/HabitsDay.tsx
+++ web/src/components/HabitsDay.tsx
@@ -7,7 +7,7 @@ import * as Popover from '@radix-ui/react-popover'
 export  function HabitsDay(){
     return(
         <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg">
-
+            
         </div>
        
     )
