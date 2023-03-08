d5562ba231740846b26e04a6b0bb94187c8d1a42
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-03-08 18:49:15 -0300

    Update SummaryTable.tsx

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -20,7 +20,6 @@ export function SummaryTable(){
     return(
         <div className="m-full flex">
             <div className="grid grid-rows-7 grid-flow gap-3">
-                
                     {weekDays.map((weekDay,i )=> {
                         return(
                             <div key={`${weekDay} -${i}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
