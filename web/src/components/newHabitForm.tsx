aeffcd4301f850425dff25590cf4de7d74cbeecc
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-03-12 18:43:12 -0300

    Update newHabitForm.tsx

--- web/src/components/newHabitForm.tsx
+++ web/src/components/newHabitForm.tsx
@@ -1,17 +1,22 @@
 import { Check } from "phosphor-react";
 import *as  CheckBox from '@radix-ui/react-checkbox'
+import { FormEvent } from "react";
 
+const dayWeeks =[
+    'Domingo',
+    'Segunda-feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sabado'
+]
 export function NewHabitForm(){
-    const dayWeeks =[
-        'segunda-feira',
-        'terça-feira',
-        'quarta-feira',
-        'quinta-feira',
-        'sexta-feira',
-        'sabado'
-    ]
+    function createNewHabit(event: FormEvent){
+        event.preventDefault()
+    }
     return(
-        <form className="w-full flex flex-col mt-6">
+        <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
             <label htmlFor="title" className="font-semibold loading-tight">
                 Qual é o seu comportamento?
             </label>
@@ -21,15 +26,16 @@ export function NewHabitForm(){
             placeholder="ex.: Exercicios, dormir be,, etc..."
             autoFocus
             className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+            
             />
 
             <label htmlFor=""  className="font-semibold loading-tight mt-4">
                 Qual a recorrencia?
             </label>
             {dayWeeks.map((days, index)=>(
-                <div>
-                      <div className="flex flex-col gap-2 mt-3">
-                <CheckBox.Root
+        
+                <div className="flex flex-col gap-2 mt-3" key={days}>
+                        <CheckBox.Root
                         className='flex items-center gap-3 group'
                     >
                         <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
@@ -42,7 +48,6 @@ export function NewHabitForm(){
                         </span>
                     </CheckBox.Root>
             </div>
-                </div>
             ))}
           
 
